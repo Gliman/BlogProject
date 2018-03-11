b3f877c33d1cee98edfd1dc51f158a35444cbc60
Gliman <ponkrkat@gmail.com>
2018-03-11 22:34:46 +0700

    makemigrations

--- project1/first_app/models.py
+++ project1/first_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class Topic(models.Model):
+    top_name = models.CharField(max_length = 264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+class Webpage(models.Model)
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264, unique= True)
+    url = models.URLField(unique= True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DataField()
+
+    def __str__(self):
+        return str(self.date)
+
 # Create your models here.
--- project1/first_app/views.py
+++ project1/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index (request):
-    my_dict = {'insert_me':"Hello, this text from views.py"}
+    my_dict = {'insert_content':"Hello, this text from views.py"}
     return render(request, 'index.html', context= my_dict)
