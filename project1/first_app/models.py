1e39253341569a9018071eb94b275d45ff4efd5e
Gliman <ponkrkat@gmail.com>
2018-03-13 21:15:00 +0700

    success migrations

--- project1/first_app/models.py
+++ project1/first_app/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
 class Topic(models.Model):
-    top_name = models.CharField(max_length = 264, unique=True)
+    top_name = models.CharField(max_length=264,unique=True)
 
     def __str__(self):
         return self.top_name
 
-class Webpage(models.Model)
-    topic = models.ForeignKey(Topic)
-    name = models.CharField(max_length=264, unique= True)
-    url = models.URLField(unique= True)
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.DO_NOTHING,)
+    name = models.CharField(max_length=264,unique=True)
+    url = models.URLField(unique=True)
 
     def __str__(self):
         return self.name
 
 class AccessRecord(models.Model):
-    name = models.ForeignKey(Webpage)
-    date = models.DataField()
+    name = models.ForeignKey(Webpage, on_delete=models.DO_NOTHING,)
+    date = models.DateField()
 
     def __str__(self):
         return str(self.date)
+
 
 # Create your models here.
