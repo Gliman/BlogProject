342682032b2f83ff5381a4d08d711c3b95e8e7c7
Gliman <ponkrkat@gmail.com>
2018-03-22 20:58:14 +0700

    valid func

--- Django_forms/basicforms/basicapp/forms.py
+++ Django_forms/basicforms/basicapp/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.core import validators
 
+def check_for_z(value):
+    if value[0].lower() !='z':
+        raise forms.ValidationError("Name needs to start with z ")
+
+
 
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget = forms.Textarea)
     botcatcher = forms.CharField(required = False,
