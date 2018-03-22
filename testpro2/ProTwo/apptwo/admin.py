68a9644864a6f714427ff06caf029df33e073db8
Gliman <ponkrkat@gmail.com>
2018-03-23 01:46:34 +0700

    model forms

--- testpro2/ProTwo/apptwo/admin.py
+++ testpro2/ProTwo/apptwo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from apptwo.models import User
+# Register your models here.
+
 
 
 admin.site.register(User)
--- testpro2/ProTwo/apptwo/views.py
+++ testpro2/ProTwo/apptwo/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from apptwo.models import User
+from apptwo.forms import NewUserForm
 
 # Create your views here.
 def index(request):
     return render(request, 'apptwo/index.html')
 
 def users(request):
+    form = NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save(commit = True)
+            return index(request)
+        else:
+            print('Error from invalid')
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'apptwo/users.html', context = user_dict)
+    return render(request, 'apptwo/users.html', {'form':form})
