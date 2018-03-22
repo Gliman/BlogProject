59abee9e864cdf73bad96b950d3e1bddd32d6462
Gliman <ponkrkat@gmail.com>
2018-03-22 23:22:02 +0700

    verify email

--- Django_forms/basicforms/basicapp/forms.py
+++ Django_forms/basicforms/basicapp/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 from django.core import validators
 
-def check_for_z(value):
-    if value[0].lower() !='z':
-        raise forms.ValidationError("Name needs to start with z ")
-
-
 
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label = 'Enter your email again:')
     text = forms.CharField(widget = forms.Textarea)
-    botcatcher = forms.CharField(required = False,
-                                widget = forms.HiddenInput,
-                                validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Make sure emails match!")
